08058e9a64152bc458d2bd45468c85827c1cf7ef
Aishwarya Mary Mathew <aishwaryamary.mathew@publicissapient.com>
2025-03-17 04:00:02 +0530

    removed sensitive info

--- app/auth/AuthConfig.js
+++ app/auth/AuthConfig.js
@@ -1,9 +1,6 @@
-const CLIENT_ID_WEB =
-  "992933353705-a8h77av91vrpajeiu2dbk91r9ususr2n.apps.googleusercontent.com";
-const CLIENT_ID_IOS =
-  "992933353705-rl2dp2echrr36bvt3gmh5rupef6rol0s.apps.googleusercontent.com";
-const CLIENT_ID_ANDROID =
-  "992933353705-s048d5c9kgmlbkfq494i8q81lmoggg82.apps.googleusercontent.com";
+const CLIENT_ID_WEB = "";
+const CLIENT_ID_IOS = "";
+const CLIENT_ID_ANDROID = "";
 
 export default {
   CLIENT_ID_WEB,
